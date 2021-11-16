4f2f0a2eed0c6235542edc1196fe82c9be559e2f
Princess <adwoaasante88@gmail.com>
2021-11-16 14:38:37 +0000

    payment

--- functionalities/payment.php
+++ functionalities/payment.php
@@ -1,311 +1,148 @@
+
+
 <!DOCTYPE html>
-
 <html lang="en">
-
   <head>
-
     <meta charset="UTF-8" />
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
     <!-- Bootstrap -->
-
     <link
-
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-
       rel="stylesheet"
-
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-
       crossorigin="anonymous"
-
     />
-
     <!-- Framework CSS -->
-
     <link rel="stylesheet" href="../assets/css/main.css" />
-
     <link rel="stylesheet" href="../assets/css/login.css" />
 
- 
-
     <title>Subscription</title>
-
     <link
-
       rel="shortcut icon"
-
       href="../images/Logo Icon.ico"
-
       type="image/x-icon"
-
     />
-
   </head>
-
   <body>
-
-      <br>
-
-      <br>
-
+      <div class="container">
+        
     <header class="major" style="text-align: center;">
-
-        <h2>Student Friendly Packages!</h2>
-
+        <h1>Student Friendly Packages!</h1>
     </header>
+    <div class="">
 
-    <div class="float-container">
-
- 
-
-        <div class="right-half col-md-6 mx-auto my-auto">
-
-          <form action="POST" method="post" id="login-form">
-
-            <!-- Username -->
-
-            <div class="float-child">
-
-                Weekly 10GHC/week
-
-                <a href="../functionalities/payment.php">
-
-                <input type="button" value="Purchase" />
-
-                </a>
-
-            </div>
-
- 
-
-            <div class="float-child">
-
-                Weekly 10GHC/week
-
-                <a href="../functionalities/payment.php">
-
-                <input type="button" value="Purchase" />
-
-                </a>
-
-            </div>
-
- 
-
-            <div class="float-child">
-
-                Weekly 10GHC/week
-
-                <a href="../functionalities/payment.php">
-
-                <input type="button" value="Purchase" />
-
-                </a>
-
-            </div>
-
- 
-
-          </form>
-
- 
-
-        </div>
-
-       
-
-      </div>
-
-    </div>
-
- 
-
-    <div class="row">
-
-             
-
-              <div class="col-xl-3 col-sm-6">
-
+        <div class="mx-auto my-auto">
+        <div class="row">
+              <div class="col-md-4 col-sm-12">
                 <div class="card bg-success text-white mb-4">
 
-                <img class="card-img-top" src="../images/building.jpg" alt="Card image cap">
-
-                  <div class="card-body">Weekly <br>10GHC/week</div>
-
-                  <div class="card-footer d-flex align-items-center justify-content-between"5>
-
- 
-
-                    <div class="small text-white">
-
-                      <i class="fas fa-angle-right"></i>
-
-                    </div>
-
-                  </div>
-
-                </div>
-
-               
-
-              </div>
-
-              <div class="col-xl-3 col-sm-6">
-
-                <div class="card bg-danger text-white mb-4">
-
-                  <div class="card-body">Danger Card</div>
-
+                  <div class="card-body">Success Card</div>
                   <div
-
                     class="
-
                       card-footer
-
                       d-flex
-
                       align-items-center
-
                       justify-content-between
-
                     "
-
                   >
-
                     <a class="small text-white stretched-link" href="#"
-
                       >View Details</a
-
                     >
-
                     <div class="small text-white">
-
                       <i class="fas fa-angle-right"></i>
-
                     </div>
-
                   </div>
-
                 </div>
-
               </div>
-
+              <div class="col-md-4 col-sm-12">
+                <div class="card bg-success text-white mb-4">
+               
+                  <div class="card-body">Success Card</div>
+                  <div
+                    class="
+                      card-footer
+                      d-flex
+                      align-items-center
+                      justify-content-between
+                    "
+                  >
+                    <a class="small text-white stretched-link" href="#"
+                      >View Details</a
+                    >
+                    <div class="small text-white">
+                      <i class="fas fa-angle-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              
+              
             </div>
+        
+      </div>
+    </div>
 
- 
+    
 
-    <!-- Form -->
-
-    <h3>Form</h3>
-
- 
-
-<form method="post" action="../actions/add_to_cart.php">
-
+<<<<<<< HEAD
+=======
+<form method="post" action="../view/cart.php">
   <div class="row gtr-uniform">
-
     <div class="col-6 col-12-xsmall">
-
       <input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
-
     </div>
-
     <div class="col-6 col-12-xsmall">
-
       <input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
-
     </div>
-
     <!-- Break -->
-
     <div class="col-12">
-
       <select name="demo-category" id="demo-category">
-
         <option value="">- Category -</option>
-
         <option value="1">Weekly</option>
-
         <option value="2">Monthly</option>
-
         <option value="3">Yearly</option>
-
       </select>
-
     </div>
-
     <!-- Break -->
-
     <div class="col-6 col-12-small">
-
       <input type="checkbox" id="demo-copy" name="demo-copy">
-
       <label for="demo-copy">Email me a copy</label>
-
     </div>
-
     <div class="col-6 col-12-small">
-
       <input type="checkbox" id="demo-human" name="demo-human" checked>
-
       <label for="demo-human">I am a human</label>
-
     </div>
-
     <!-- Break -->
-
     <div class="col-12">
-
       <ul class="actions">
-
         <li><input type="submit" value="Subscribe" class="primary" /></li>
-
         <li><input type="reset" value="Reset" /></li>
-
       </ul>
-
     </div>
-
   </div>
-
 </form>
+>>>>>>> cbececf6e6faf27b6ba9939a01f0d743af248577
 
- 
-
-   
-
- 
+    
+      </div>
 
     <!-- Bootstrap -->
-
     <script
-
       src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-
       integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-
       crossorigin="anonymous"
-
     ></script>
-
     <script
-
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-
       integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-
       crossorigin="anonymous"
-
     ></script>
-
   </body>
-
 </html>
 
- 
 
 <form>
+
+
